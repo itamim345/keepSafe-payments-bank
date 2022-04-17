03ebd9762055b015d2b537ab052cae2a03c02250
AF Tamim <itamim345@gmail.com>
2022-04-17 14:17:36 +0530

    applied js on deposited

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,16 @@
 document.getElementById("deposit-btn").addEventListener("click", function(){
-    const depoCurrentAmount = document.getElementById("deposit").value;
-    const depoAmount = document.getElementById("deposit-field").innerText = depoCurrentAmount;
+    //show deposit
+    const depoPre= document.getElementById("show-deposit");
+    const depoPreAmountText = depoPre.innerText;
+    const depoPreAmount = parseFloat(depoPreAmountText);
+    //take deposit input
+    const depoAmountInput = document.getElementById("deposit");
+    const depositedAmountText= depoAmountInput.value;
+    const depositedAmount = parseFloat(depositedAmountText);
+    //calculate total amount
+    const depoCurrentAmount = (depoPreAmount+depositedAmount);
+    //changed the previous value to show updated calculated value
+    depoPre.innerText = depoCurrentAmount;
+    //to clear the input field after deposit
+    depoAmountInput.value = '';
     });
