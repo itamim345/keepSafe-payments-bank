6603a3579069354404b0d1827bbcda2672567c96
AF Tamim <itamim345@gmail.com>
2022-05-04 19:20:12 +0530

    some usefull validation done

--- dashboard.js
+++ dashboard.js
@@ -18,34 +18,57 @@ function totalAmount (showBalanceId,inputedAmount){
     depoPre.innerText = depoCurrentAmount;
 }
 
-document.getElementById("deposit-btn").addEventListener("click", function(){  
-    //take user deposit input
-    const depositedAmount = userInputAmount('deposit');
-    //calculate & show total deposited amount
-    totalAmount('show-deposit',depositedAmount);
+function updateMainBalance(transacAmount,isAdd){
     // take previous Main balance
     const prebalance = document.getElementById("show-balance");
     const prebalanceText = prebalance.innerText;
     const prebalanceAmount = parseFloat(prebalanceText);
-    //calculate updating Main balance
-    const updatedBalance = (prebalanceAmount+depositedAmount);
-    //changed the previous Main balance to show updated calculated balance
-    prebalance.innerText = updatedBalance;
+    if(isAdd == true){
+        //calculate updating Main balance
+        const updatedBalance = (prebalanceAmount+transacAmount);
+        //changed the previous Main balance to show updated calculated balance
+        prebalance.innerText = updatedBalance;
+    }else{
+        //calculate updated Main balance after withdraw
+        const updatedBalanceAmount = prebalanceAmount-transacAmount;
+        //changed the previous Maun balance to show updated Main balance amount after withdraw
+        prebalance.innerText = updatedBalanceAmount;
+    }
+    
+}
+
+document.getElementById("deposit-btn").addEventListener("click", function(){  
+    //take user deposit input
+    const depositedAmount = userInputAmount('deposit');
+
+    if(depositedAmount > 0){
+        //calculate & show total deposited amount
+        totalAmount('show-deposit',depositedAmount);
+        //calculate and changed the previous Main balance to show updated calculated balance
+        updateMainBalance(depositedAmount,true);
+    }else{
+        alert("please enter a valid amount..")
+    }
     });
 
     document.getElementById("withdraw-btn").addEventListener("click", function(){
         // take user withdraw input
         const withdrawAmount = userInputAmount('withdraw');
-        //calculate & show total deposited amount
-        totalAmount("show-withdraw",withdrawAmount)
-        //take Main balance 
-        const preWithdrawBalance = document.getElementById("show-balance");
-        const preWithdrawBalanceText = preWithdrawBalance.innerText;
-        const preWithdrawBalanceAmount = parseFloat(preWithdrawBalanceText);
-        //calculate updated Main balance after withdraw
-        const updatedBalanceAmount = preWithdrawBalanceAmount-withdrawAmount;
-        //changed the previous Maun balance to show updated Main balance amount after withdraw
-        preWithdrawBalance.innerText = updatedBalanceAmount;
+        // take previous Main balance to ensure the inputed withdraw amount is availalable in account or not
+        const prebalance = document.getElementById("show-balance");
+        const prebalanceText = prebalance.innerText;
+        const prebalanceAmount = parseFloat(prebalanceText);
+
+        if( (withdrawAmount > 0) && (withdrawAmount <= prebalanceAmount) ){
+            //calculate & show total withdrawn amount
+            totalAmount("show-withdraw",withdrawAmount)
+            //calculate and changed the previous Main balance to show updated calculated balance
+            updateMainBalance(withdrawAmount,false);
+        }if(withdrawAmount > prebalanceAmount){
+            alert("You don't have sufficient fund to withdraw");
+        }else{
+            alert("please enter valid amount!")
+        }
     })
 
 //log out page redirec to log-in page
