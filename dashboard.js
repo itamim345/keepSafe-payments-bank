e27c570e695cd36010a36598990d609b72c81254
AF Tamim <itamim345@gmail.com>
2022-05-04 17:42:41 +0530

    removed duplicate code using function

--- dashboard.js
+++ dashboard.js
@@ -1,55 +1,51 @@
-document.getElementById("deposit-btn").addEventListener("click", function(){
-    //show deposit
-    const depoPre= document.getElementById("show-deposit");
+function userInputAmount(inputId){
+    //take user deposit input
+    const inputField = document.getElementById(inputId);
+    const inputText= inputField.value;
+    const inputAmount = parseFloat(inputText);
+    //to clear the input field after deposit
+    inputField.value = '';
+    return inputAmount;
+}
+function totalAmount (showBalanceId,inputedAmount){
+    //take previous deposit
+    const depoPre= document.getElementById(showBalanceId);
     const depoPreAmountText = depoPre.innerText;
-    const depoPreAmount = parseFloat(depoPreAmountText);
-    //take deposit input
-    const depoAmountInput = document.getElementById("deposit");
-    const depositedAmountText= depoAmountInput.value;
-    const depositedAmount = parseFloat(depositedAmountText);
+    const depoPreAmount = parseFloat(depoPreAmountText);   
     //calculate total amount
-    const depoCurrentAmount = (depoPreAmount+depositedAmount);
+    const depoCurrentAmount = (depoPreAmount+inputedAmount);
     //changed the previous deposited value to show updated calculated value
     depoPre.innerText = depoCurrentAmount;
+}
 
-    // show balance
+document.getElementById("deposit-btn").addEventListener("click", function(){  
+    //take user deposit input
+    const depositedAmount = userInputAmount('deposit');
+    //calculate & show total deposited amount
+    totalAmount('show-deposit',depositedAmount);
+    // take previous Main balance
     const prebalance = document.getElementById("show-balance");
     const prebalanceText = prebalance.innerText;
     const prebalanceAmount = parseFloat(prebalanceText);
-    //calculate updating balance
+    //calculate updating Main balance
     const updatedBalance = (prebalanceAmount+depositedAmount);
-    //changed the previous balance to show updated calculated balance
+    //changed the previous Main balance to show updated calculated balance
     prebalance.innerText = updatedBalance;
-    
-    //to clear the input field after deposit
-    depoAmountInput.value = '';
     });
 
     document.getElementById("withdraw-btn").addEventListener("click", function(){
-        //show withdraw
-        const preWithdraw = document.getElementById("show-withdraw");
-        const preWithdrawText = preWithdraw.innerText;
-        const preWithdrawAmount = parseFloat(preWithdrawText);
-        // take withdraw input
-        const withdraw = document.getElementById("withdraw");
-        const withdrawText = withdraw.value;
-        const withdrawAmount = parseFloat(withdrawText);
-        //calculate update
-        const totalWithdraw = (preWithdrawAmount+withdrawAmount);
-        //changed the previous withdraw to show updated withdraw balance
-        preWithdraw.innerText = totalWithdraw;
-
-        //show balance 
+        // take user withdraw input
+        const withdrawAmount = userInputAmount('withdraw');
+        //calculate & show total deposited amount
+        totalAmount("show-withdraw",withdrawAmount)
+        //take Main balance 
         const preWithdrawBalance = document.getElementById("show-balance");
         const preWithdrawBalanceText = preWithdrawBalance.innerText;
         const preWithdrawBalanceAmount = parseFloat(preWithdrawBalanceText);
-        //calculate updated balance after withdraw
+        //calculate updated Main balance after withdraw
         const updatedBalanceAmount = preWithdrawBalanceAmount-withdrawAmount;
-        //changed the previous balance to show updated balance amount after withdraw
+        //changed the previous Maun balance to show updated Main balance amount after withdraw
         preWithdrawBalance.innerText = updatedBalanceAmount;
-
-        // clear the input field after taking withdraw amount
-        withdraw.value = '';
     })
 
 //log out page redirec to log-in page
